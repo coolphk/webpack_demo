27d009ebd228cd864535a88ec440141bd33d5b9e
Administrator <25145168@qq.com>
2019-04-01 19:48:19 +0800

    htmlwebpacktemplate

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import printMe from './print'
+
+function component() {
+    var element = document.createElement('div');
+
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
+    return element;
+}
+
+document.body.appendChild(component());
